a35b4ade21c1a7c5a1d77fba12c17d53862b4b23
parisgirl16 <parisgirl16@sbcglobal.net>
2018-11-14 14:33:13 +0000

    Done.

--- puppies.rb
+++ puppies.rb
@@ -2,36 +2,36 @@ def no_puppies(number)
   if number > 0
     puts "No more puppies!"
   elsif number <= 0
-    puts "Well done, Rachel!"
+    puts "Good job, Rachel!"
   end
 end
-no_puppies(1)
+no_puppies(0)
 
 def less_puppies(number)
   if number<3
     puts "Good job!"
   elsif number==3
-    puts "Slow down!"
+    puts "Slow down! No more puppies."
   elsif number>3
-    puts "Get back to animal restriction!"
-end
+    puts "Get back to your animal goals!"
+  end
 end
 less_puppies(2)
 
 def some_puppies(number,maximum)
   if maximum>number
-    puts "Good job!"
+    puts "Good job, Rachel!"
   elsif maximum<number
-    puts "Go back to puppy restriction."
+    puts "Go back your animal goals!"
+  end
 end
-end
-some_puppies(3,5)
+some_puppies(2,6)
 
-def new_animal(puppies,cats)
+def both_animals(puppies,cats)
   if puppies == 0 && cats == 0
     puts "Good job!"
   elsif puppies > 1 && cats > 1 
     puts "Slow down!"
   end
 end
-new_animal(3,3)
+both_animals(0,0)
