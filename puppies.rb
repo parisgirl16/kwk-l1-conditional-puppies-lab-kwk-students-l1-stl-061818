1ae5d5cb333867bd762c65704b6d357a14872637
parisgirl16 <parisgirl16@sbcglobal.net>
2018-11-14 14:32:58 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -11,27 +11,27 @@ def less_puppies(number)
   if number<3
     puts "Good job!"
   elsif number==3
-    puts "Slow down!"
-  elsif number>3
     puts "Slow down! No more puppies."
+  elsif number>3
+    puts "Get back to your animal goals!"
   end
 end
 less_puppies(2)
 
 def some_puppies(number,maximum)
   if maximum>number
-    puts "Good job!"
+    puts "Good job, Rachel!"
   elsif maximum<number
-    puts "Go back to puppy restriction."
+    puts "Go back your animal goals!"
   end
 end
 some_puppies(2,6)
 
-def new_animal(puppies,cats)
+def both_animals(puppies,cats)
   if puppies == 0 && cats == 0
     puts "Good job!"
   elsif puppies > 1 && cats > 1 
     puts "Slow down!"
   end
 end
-new_animal(0,0)
+both_animals(0,0)
